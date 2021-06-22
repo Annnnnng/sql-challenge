0d36e661693b1e51b1900e62bed2d0ad02cfa60b
unknown <angli5166@gmail.com>
2021-06-22 17:40:20 -0500

    homework

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -42,20 +42,20 @@ WHERE emp_title_id IN
 	);
 
 
--- 4.
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name
 
 SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
 FROM ((employees
 INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no)
 INNER JOIN department ON dept_employees.dept_no = department.dept_no);
 
--- 5.
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
--- 6.
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
 SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
 FROM ((employees
@@ -63,7 +63,7 @@ INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no)
 INNER JOIN department ON dept_employees.dept_no = department.dept_no)
 WHERE dept_name = 'Sales';
 
--- 7.
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
 FROM ((employees
@@ -71,7 +71,7 @@ INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no)
 INNER JOIN department ON dept_employees.dept_no = department.dept_no)
 WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
--- 8.
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
 SELECT last_name, COUNT(last_name)
 FROM employees
