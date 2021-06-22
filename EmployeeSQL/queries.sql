2089f65a187c0b36b5690a8ded27ce006e3a8c5b
unknown <angli5166@gmail.com>
2021-06-21 22:55:23 -0500

    homework

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,81 @@
+-- 1. List the details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no = salaries.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE DATE_PART('year', hire_date) = 1986;
+
+
+-- select manager details
+SELECT title_id FROM titles
+WHERE title = 'Manager';
+SELECT emp_no, last_name, first_name
+FROM employees
+WHERE emp_title_id IN
+(
+	SELECT title_id FROM titles
+	WHERE title = 'Manager'
+);
+
+-- 3. select manager details with department info
+
+SELECT titles.title, dept_employees.dept_no, department.dept_name,
+	employees.emp_no, employees.last_name, employees.first_name	
+FROM (((employees
+INNER JOIN titles ON employees.emp_title_id = titles.title_id)
+INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no)
+INNER JOIN department ON dept_employees.dept_no = department.dept_no)
+WHERE emp_title_id IN    
+	(	
+		SELECT title_id FROM titles
+	  	WHERE title = 'Manager'
+	);
+
+
+-- 4.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
+FROM ((employees
+INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no)
+INNER JOIN department ON dept_employees.dept_no = department.dept_no);
+
+-- 5.
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- 6.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
+FROM ((employees
+INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no)
+INNER JOIN department ON dept_employees.dept_no = department.dept_no)
+WHERE dept_name = 'Sales';
+
+-- 7.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
+FROM ((employees
+INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no)
+INNER JOIN department ON dept_employees.dept_no = department.dept_no)
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+-- 8.
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+GROUP BY (last_name)
+ORDER BY last_name DESC;
+
+
---
+++ EmployeeSQL/table schema.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE department (
+	dept_no TEXT PRIMARY KEY,
+	dept_name TEXT
+);
+
+
+CREATE TABLE dept_employees (
+	emp_no INT,
+	dept_no TEXT
+);
+
+
+CREATE TABLE dept_manager (
+	dept_no TEXT,
+	emp_no INT,
+	PRIMARY KEY (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id TEXT,
+	birth_date DATE,
+	first_name TEXT,
+	last_name TEXT,
+	sex TEXT,
+	hire_date DATE, 
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+
+CREATE TABLE salaries(
+	emp_no INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INT
+);
+
+
+CREATE TABLE titles(
+	title_id TEXT,
+	title TEXT
+);
